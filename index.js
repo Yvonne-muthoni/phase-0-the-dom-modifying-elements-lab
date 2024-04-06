dac5a072f12cd2d3c8b2a165e120d4a6ac2444c7
Yvonne Muriuki <yvonnemuriuki01@gmail.com>
2024-04-06 13:03:16 +0300

    dom-modifying

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const element=document.getElementsByName("main");
+console.log(element.name)
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Yvonne is the champion";
+const newHeaderElement = document.getElementById('main').appendChild(newHeader);
